1a40ef0c063672b8dfb4711131f30ca35da17c64
cgonzalezt <cristipu123@gmail.com>
2023-11-07 17:37:46 -0500

    addition of login register and main page

---
+++ screens/register.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import './App.js';
+
+export default function register() {
+  return (
+    <View style={styles.container}>
+      <Text>register</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
