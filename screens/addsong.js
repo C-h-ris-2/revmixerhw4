4b064c71880a19c879810139595820647933ad82
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-11-12 17:42:03 -0500

    Update addsong.js

--- screens/addsong.js
+++ screens/addsong.js
@@ -1,13 +1,30 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import addsong from './App.js';
 
 export default function AddSong() {
+
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const[rating, setRating] = useState('');
+
+  const handleAdd = () => {
+    axios
+    .post('http://172.21.76.243:8080/comp333-hw3-frontend/index.php/user/songinsert', {username, artist, song, rating})
+    .then((response) => {
+      console.log(response.data.msg);
+    })
+    .catch((error) => {
+      console.error("Error adding new song: ", error);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Add a song here mother fucker</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Add a song here mother fucker</Text>
+      <StatusBar style="auto" /> */}
+      <TextInput value={artist} placeholder={"Username"} onChangeText={(text) => setArtist(text)} autoCapitalize={"none"}/>
     </View>
   );
 }
