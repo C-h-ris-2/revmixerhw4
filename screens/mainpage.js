9edb9f4a24a9210c7a648622e059d23d8bddb712
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-11-14 10:46:02 -0500

    I was able to move the username across and make the mainpage scrollable

--- screens/mainpage.js
+++ screens/mainpage.js
@@ -1,39 +1,49 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Flatlist} from 'react-native';
-import {DataTable } from "react-native-paper";
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+import { DataTable } from "react-native-paper";
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// import mainpage from './App.js';
-
 export default function MainPage() {
+  const [posts, setPosts] = useState([]);
+  const [username, setUsername] = useState('');
 
   const query = async () => {
-    let response = await axios.get("http://172.21.76.243:8080/comp333-hw3-frontend/index.php/user/songlist", {});
-    if (response.data.code === 0) {
+    try {
+      let response = await axios.get("http://172.21.76.243:8080/comp333-hw3-frontend/index.php/user/songlist", {});
+      if (response.data.code === 0) {
         setPosts(response.data.data);
-    } else {
+      } else {
         console.error('Error fetching data');
-        alert(response.data.msg); 
+        alert(response.data.msg);
+      }
+    } catch (error) {
+      console.error('Error fetching data', error);
     }
-}
+  }
 
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const storedUsername = await AsyncStorage.getItem("username");
+        if (storedUsername) {
+          setUsername(storedUsername);
+        }
+      } catch (error) {
+        console.error('Error fetching username', error);
+      }
+    };
 
-const [posts, setPosts] = useState([]);
-
-
-useEffect(() => {
-  query();
-})
+    fetchUsername();
+    query();
+  }, []);
 
   return (
-
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.container}>
-            {/* <Text title="username">Logged in as: {AsyncStorage.getItem("username")}</Text> */}
+      <Text>You are logged in as: {username}</Text>
 
-      {/* <Text>mainpage</Text>
-      <StatusBar style="auto" /> */}
       <DataTable>
         <DataTable.Header>
           <DataTable.Title>ID</DataTable.Title>
@@ -42,8 +52,8 @@ useEffect(() => {
           <DataTable.Title>Song</DataTable.Title>
           <DataTable.Title>Rating</DataTable.Title>
         </DataTable.Header>
-        {posts.map((r,i) => (
-          <DataTable.Row>
+        {posts.map((r, i) => (
+          <DataTable.Row key={i}>
             <DataTable.Cell>{r.id}</DataTable.Cell>
             <DataTable.Cell>{r.username}</DataTable.Cell>
             <DataTable.Cell>{r.artist}</DataTable.Cell>
@@ -53,9 +63,9 @@ useEffect(() => {
         ))}
       </DataTable>
     </View>
+    </ScrollView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
